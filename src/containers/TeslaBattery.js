b22a9c86ff06decea194e548f3d7bd7bede119da
Marwen Magri <marwen.magri@gomycode.co>
2020-05-14 23:28:25 +0000

    state mang props test

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -3,11 +3,22 @@ import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
 class TeslaBattery extends React.Component {
+    state = {
+        carstats: [],
+        config: {
+          speed: 55,
+          temperature: 20,
+          climate: true,
+          wheels: 19
+        }
+      }
   render() {
+   // takes out required values and create references to them
+    const { config } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
-        <TeslaCar  />
+        <TeslaCar wheelsize={config.wheels} />
         <TeslaNotice />
       </form>
     )
