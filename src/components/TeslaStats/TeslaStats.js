75cc7cdba3bc2f6aad70fb4ba10b39c164873490
Marwen Magri <marwen.magri@gomycode.co>
2020-05-14 23:57:53 +0000

    carmodel & config setup

---
+++ src/components/TeslaStats/TeslaStats.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "react";
+import "./TeslaStats.css";
+
+const TeslaStats = ({ carstats }) => {
+  const listItems = carstats.map((stat) => (
+    <li key={stat.model}>
+      <div
+        className={`tesla-stats-icon tesla-stats-icon--${stat.model.toLowerCase()}`}
+      ></div>
+      <p>{stat.miles}</p>
+    </li>
+  ));
+
+  return (
+    <div className="tesla-stats">
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
+
+TeslaStats.propTypes = {
+  carstats: PropTypes.array,
+};
+
+export default TeslaStats;
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaStats from '../components/TeslaStats/TeslaStats';
+import { getModelData } from '../services/BatteryService';
 class TeslaBattery extends React.Component {
     state = {
         carstats: [],
@@ -12,13 +14,39 @@ class TeslaBattery extends React.Component {
           wheels: 19
         }
       }
+      calculateStats = (models, value) => {
+        const dataModels = getModelData();
+        return models.map(model => {  
+          // ES6 Object destructuring Syntax,
+          // takes out required values and create references to them
+          const { speed, temperature, climate, wheels } = value;
+          const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+          return {
+            model,
+            miles
+          };
+        });
+      }
+        
+      statsUpdate() {
+        const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+        // Fetch model info from BatteryService and calculate then update state
+        this.setState({
+          carstats: this.calculateStats(carModels, this.state.config)
+        })  
+      }
+        
+      componentDidMount() {
+        this.statsUpdate(); 
+      }
   render() {
    // takes out required values and create references to them
-    const { config } = this.state;
+    const { carstats,config } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
         <TeslaCar wheelsize={config.wheels} />
+        <TeslaStats carstats={carstats}/>
         <TeslaNotice />
       </form>
     )
