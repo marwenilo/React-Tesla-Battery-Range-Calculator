578e2e839f1c3a6e87ab8570600e2499f800176e
Marwen Magri <marwen.magri@gomycode.co>
2020-05-15 00:30:58 +0000

    speed & temp inp

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import TeslaBattery from './containers/TeslaBattery';
-
-const counterDefaultVal = {
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TeslaBattery from "./containers/TeslaBattery";
+const counterDefaultVal  = {
   speed: {
     title: "Speed",
     unit: "mph",
     step: 5,
     min: 45,
-    max: 70
+    max: 70,
   },
   temperature: {
     title: "Outside Temperature",
     unit: "°",
     step: 10,
     min: -10,
-    max: 40
-  }
+    max: 40,
+  },
 };
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <TeslaBattery counterDefaultVal={counterDefaultVal}/>
-      </div>
-    );
-  }
+const App=()=> {
+  return (
+    <div className="App">
+      <Header />
+      <TeslaBattery counterDefaultVal={counterDefaultVal} />
+    </div>
+  );
 }
 
 export default App;
